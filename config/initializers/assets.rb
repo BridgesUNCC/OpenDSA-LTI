76b24fe9bbc6ddf3a2addcd4febb297b6582278b
Jackson Wonderly <jacksonwonderly@gmail.com>
2018-01-18 21:25:42 +0000

    fix asset compilation script

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,13 @@
 # config/initializers/assets.rb
 Rails.application.config.assets.precompile << Proc.new do |path|
   if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
-    full_path = Rails.application.assets.resolve(path)#.to_path
+    full_path = Rails.application.assets_manifest.find(path).first.filename
     app_assets_path = Rails.root.join('app', 'assets').to_path
     if full_path.starts_with? app_assets_path
-      # logger.info "including asset: " + full_path
+      #puts "including asset: " + full_path
       true
     else
-      # logger.info "excluding asset: " + full_path
+      #puts "excluding asset: " + full_path
       false
     end
   else
