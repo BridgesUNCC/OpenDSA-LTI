f20f8e74c9282ebff4375c070315c30b35b02f70
Jackson Wonderly <jacksonwonderly@gmail.com>
2018-01-23 21:10:48 +0000

    Fix method used to locate asset paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,17 +1,16 @@
 # config/initializers/assets.rb
+
 Rails.application.config.assets.precompile << Proc.new do |path|
   if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
-    full_path = Rails.application.assets_manifest.find_sources(path)
-    if full_path.size == nil
+    full_path = Rails.application.assets_manifest.find(path)
+    if full_path.peek == nil
       false
     else
       full_path = full_path.first.filename
       app_assets_path = Rails.root.join('app', 'assets').to_path
       if full_path.starts_with? app_assets_path
-        #puts "including asset: " + full_path
         true
       else
-        #puts "excluding asset: " + full_path
         false
       end
     end
