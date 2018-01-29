36d99fc78036c0b107a1c4bf8cce066843655d80
Jackson Wonderly <jacksonwonderly@gmail.com>
2018-01-29 21:57:00 +0000

    change asset compilation method

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,18 +1,34 @@
 # config/initializers/assets.rb
 
+# Rails.application.config.assets.precompile << Proc.new do |path|
+#   if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
+#     full_path = Rails.application.assets_manifest.find(path)
+#     if full_path.peek == nil
+#       false
+#     else
+#       full_path = full_path.first.filename
+#       app_assets_path = Rails.root.join('app', 'assets').to_path
+#       if full_path.starts_with? app_assets_path
+#         true
+#       else
+#         false
+#       end
+#     end
+#   else
+#     false
+#   end
+# end
+
 Rails.application.config.assets.precompile << Proc.new do |path|
   if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
-    full_path = Rails.application.assets_manifest.find(path)
-    if full_path.peek == nil
-      false
+    full_path = Rails.application.assets.resolve(path)
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      # logger.info "including asset: " + full_path
+      true
     else
-      full_path = full_path.first.filename
-      app_assets_path = Rails.root.join('app', 'assets').to_path
-      if full_path.starts_with? app_assets_path
-        true
-      else
-        false
-      end
+      # logger.info "excluding asset: " + full_path
+      false
     end
   else
     false
